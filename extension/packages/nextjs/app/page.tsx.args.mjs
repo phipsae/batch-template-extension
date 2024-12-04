78a9663ca14f57dde59897ac62d5d93f430289b3
phipsae <philip.krause88@yahoo.de>
2024-12-04 15:41:48 +0800

    initial commit

---
+++ extension/packages/nextjs/app/page.tsx.args.mjs
@@ -0,0 +1,9 @@
+export const externalExtensionName = `Batch X`
+
+export const description = `
+<p className="text-center text-lg">Get started by taking a look at your batch GitHub repository.</p>
+<p className="text-lg flex gap-2 justify-center">
+  <span className="font-bold">Checked in builders count:</span>
+  <span>To Be Implemented</span>
+</p>
+`
